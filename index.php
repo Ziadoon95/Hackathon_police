c00227f8afc0e5453c82c575ff111ffa99b6ff62
zaidun <zaidun10@yahoo.com>
2020-12-09 15:20:31 +0100

    u

--- index.php
+++ index.php
@@ -12,12 +12,14 @@
 <body>
 
 <div class="w3-container">
-  <h2>Pogami</h2>
-    <input type="button" name="inscription" id="" class="inscription" value="Inscription">
-    <input type="button" name="connexion" id="" class="connexion" value="Connexion">
+  <h1>Pogami</h1>
+    <input type="button" name="inscription" id="" class="buttons inscription" value="Inscription">
+    <input type="button" name="connexion" id="" class="buttons connexion" value="Connexion">
 </div>
 
 <div class="w3-content w3-display-container" style="max-width:1000px">
+  <img class="mySlides" src="./imgs/img.png" style="width:100%">
+  <img class="mySlides" src="./imgs/img.png" style="width:100%">
   <img class="mySlides" src="./imgs/img.png" style="width:100%">
 
   <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
@@ -28,6 +30,30 @@
     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
   </div>
 </div>
+
+<div>
+  <div><p>So, ready to challenge yourself ?</p></div>
+  <div class="content-first_section">
+    <p>For those already in function, a way ... </p>
+    <p>To preserve, to improve, to assimilate. 
+    But also to share his knowledge,
+    whatever its physical or theorical</p>
+  </div>
+
+  <div class="content-second_section">
+    <p>For those who have the vocation to become, a way...</p>
+    <p>To progress, to stay motivated, but also to meet and inherit
+      knowledges from skilled policeman </p>
+  </div>
+  
+
+</div>
+
+<footer>
+  <p>@copyright 2020</p>
+</footer>
+
+
 
 <script>
 var slideIndex = 1;
